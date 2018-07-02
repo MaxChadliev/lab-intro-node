bd1ff868677eed64c949d696e10fa0f38bd6a14d
Maxim Chadliev <chad0441@gmail.com>
2018-07-02 15:12:43 -0400

    hella not done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,40 @@
-var SortedList = function() {};
+class SortedList {
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
+add(x) {
+  this.items.push(x);
+  this.items.sort();
+  this.length = this.items.length;
+  }
+  get(n) {
+    return this.items[n - 1];
+  }
+  max () {
+    return this.items[this.length - 1];
+  }
+  min () {
+    return this.items[0];
+  }
+}
+
+
+
+
+
+
+// SortedList.prototype.add = function(item) {
+
+
+
+
+
+
+// SortedList.prototype.get     = function(pos) {}
+// SortedList.prototype.max     = function() {}
+// SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
 
